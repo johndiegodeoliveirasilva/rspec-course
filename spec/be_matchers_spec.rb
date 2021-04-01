b7564926e3592fd77a9c4598f202a52bc46e2959
John Diego <johnoliveirasilva9@gmail.com>
2021-03-31 21:41:51 -0300

    Be Matchers

---
+++ spec/be_matchers_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe 'be matchers' do
+  it 'can test for truthiness' do
+    expect(true).to be_truthy
+    expect('Hello').to be_truthy
+  end
+
+  it 'can test for falsiness' do
+    expect(false).to be_falsy
+    expect(nil).to be_falsy
+  end
+
+  it 'can test for nil' do
+    expect(nil).to be_nil
+
+    my_hash = { a: 5 }
+    expect(my_hash).not_to be_nil
+  end
+end
